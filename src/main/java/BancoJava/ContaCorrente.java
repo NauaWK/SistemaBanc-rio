abb27e0dbc71dfe37fa66adc2fd0505f070ce632
Nauã_WK <106595596+NauaWK@users.noreply.github.com>
2025-08-24 12:41:32 -0300

    Update ContaCorrente.java

--- src/main/java/BancoJava/ContaCorrente.java
+++ src/main/java/BancoJava/ContaCorrente.java
@@ -3,8 +3,7 @@ package BancoJava;
 import java.math.BigDecimal;
 
 public class ContaCorrente extends ContaBanco {
-    
-   
+     
     public ContaCorrente(int numConta, String tipoConta, String nomeDono, BigDecimal saldo){
         super(numConta, tipoConta, nomeDono, saldo);
         this.saldo = saldo.add(BigDecimal.valueOf(50));
